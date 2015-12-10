d98425a2b446ac6897449140045d57662903ed02
Harshit Thukral <H.thakral369@gmail.com>
2015-12-10 14:24:32 +0530

    adds some basic test programs for opencv

---
+++ test-programs/display_image/CMakeFiles/load_images.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/load_images.dir/load_images.cpp.o"
+  "load_images.pdb"
+  "load_images"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/load_images.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ test-programs/feature_detection/CMakeFiles/featurepoints.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/featurepoints.dir/featurepoints.cpp.o"
+  "featurepoints.pdb"
+  "featurepoints"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/featurepoints.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
