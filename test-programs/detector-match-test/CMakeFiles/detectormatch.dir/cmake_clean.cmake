f79939388a524e05c30e8ba0e6b1dafad8edaa07
Harshit Thukral <H.thakral369@gmail.com>
2015-12-11 22:08:34 +0530

    adds new test program for keypoint matcher

---
+++ test-programs/detector-match-test/CMakeFiles/detectormatch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/detectormatch.dir/detectormatch.cpp.o"
+  "detectormatch.pdb"
+  "detectormatch"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/detectormatch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
